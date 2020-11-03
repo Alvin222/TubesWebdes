d74ab6ce8b6375d86345bb9cbf2b5e6e784efb2e
Alvin222 <33701232+Alvin222@users.noreply.github.com>
2020-11-03 19:23:49 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,15 +6,15 @@ var content = document.querySelector(".content");
 /*Buat media hp cuman belom responsive, harus di reload*/
 if (media.matches){
     index = 0;
-	content.innerHTML += "<img class='featured_image' src='" + img[index] + "' width='291' height='700'/>" ;
+	content.innerHTML += "<img class='index_image' src='" + img[index] + "' width='291' height='700'/>" ;
 	setInterval(function(){
 		if (index > img.length-1) index = 0;
-		document.querySelector(".featured_image").src = img[index];
+		document.querySelector(".index_image").src = img[index];
 		index++;
 	},2000)
 /*Buat media laptop, cuman belom responsive, harus di reload*/
 } else {
 	for (var i=0; i < img.length; i++){
-		content.innerHTML += "<img class='featured_image' src='" + img[i] + "' width='" + (content.clientWidth/4-((1.1/100) * content.clientWidth )) + "' height='700'/>";
+		content.innerHTML += "<img class='index_image' src='" + img[i] + "' width='" + (content.clientWidth/4-((1.1/100) * content.clientWidth )) + "' height='"+((content.clientWidth/4-((1.1/100) * content.clientWidth ))*2)+"'/>";
 	}
 }
