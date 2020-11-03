088bf670d36692281e5dcae5464209f10e78ac24
Alvin222 <33701232+Alvin222@users.noreply.github.com>
2020-11-03 18:46:15 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+alert("JS WORK");
+
+const img = ["images/drummer.jpg","images/guitarist.jpg","images/pianist.jpg","images/singer.jpg"];
+var media = window.matchMedia("(max-width: 700px)");
+var content = document.querySelector(".content");
+
+if (media.matches){
+    index = 0;
+	content.innerHTML += "<img class='featured_image' src='" + img[index] + "' width='291' height='700'/>" ;
+	setInterval(function(){
+		if (index > img.length-1) index = 0;
+		document.querySelector(".featured_image").src = img[index]
+		index++;
+	},2000)
+} else {
+	for (var i=0; i < img.length; i++){
+		content.innerHTML += "<img class='featured_image' src='" + img[i] + "' width='323' height='700'/>";
+	}
+}
