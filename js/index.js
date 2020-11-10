d637d4bc54b227f86792e3bbb3eaf1e0cb32e14d
Alvin222 <33701232+Alvin222@users.noreply.github.com>
2020-11-10 16:48:24 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 const img = ["images/drummer.jpg","images/guitarist.jpg","images/pianist.jpg","images/singer.jpg"];
 var media = window.matchMedia("(max-width: 700px)");
 var content = document.querySelector(".content");
+index = 0;
 
 /*Multiple picture content*/
-/*Buat media hp cuman belom responsive, harus di reload*/
-if (media.matches){
-    index = 0;
-	content.innerHTML += "<img class='index_image' src='" + img[index] + "' width='291' height='700'/>" ;
-	setInterval(function(){
-		if (index > img.length-1) index = 0;
-		document.querySelector(".index_image").src = img[index];
-		index++;
-	},2000)
-/*Buat media laptop, cuman belom responsive, harus di reload*/
-} else {
-	for (var i=0; i < img.length; i++){
-		content.innerHTML += "<img class='index_image' src='" + img[i] + "' width='" + (content.clientWidth/4-((1.1/100) * content.clientWidth )) + "' height='"+((content.clientWidth/4-((1.1/100) * content.clientWidth ))*2)+"'/>";
+$(document).ready(()=>{
+	if ($(window).width() <= 700) {
+		content.innerHTML += "<img class='index_image' src='" + img[index] + "' width='291' height='700'/>" ;
+	} else {
+		content.innerHTML += "<img class='index_image' src='" + img[index] + "' width='" + (1290) + "' height='"+800+"'/>";
 	}
-}
+	setInterval(()=>{
+		if (index > img.length-1) index = 0;
+		$(".index_image").fadeOut(600);
+		$(".index_image").attr('src',img[index]);
+		$(".index_image").fadeIn(1300);
+		index++
+	},2000);
+});
